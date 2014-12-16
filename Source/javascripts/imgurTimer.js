a05b77a99d8651480c84e7663fff9cd4f76c3174
Robert Benapfl <rabsax@comcast.net>
2014-12-16 11:05:50 -0800

    add manifest, icon, html, js, css file structure

---
+++ Source/javascripts/imgurTimer.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+	//only runs when window is open via DOMContentLoaded
+	// var counter = 0
+	// var addToCounter = function() {
+	// 	counter += 1
+	// 	document.getElementById('timeDisplay').innerText = counter
+	// }
+	// setInterval(function(){ addToCounter()}, 3000);
+
+	document.getElementById('timeDisplay').innerText = 'Hi'
+})
