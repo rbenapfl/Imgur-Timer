710fafca0b095be67c73ad5c4c1166b610bff58d
Robert Benapfl <rabsax@comcast.net>
2014-12-17 11:40:18 -0800

    add url polling feature to deal with changing tabs to other sites or closing chrome

--- Source/javascripts/background.js
+++ Source/javascripts/background.js
@@ -1,5 +1,18 @@
-var number = 2
+var hours = 0
+var minutes = 0
+var seconds = 0
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	number += 1
-	sendResponse({message: number})
+	if (request.greeting === 'give me the time')
+	sendResponse({hours: hours, minutes: minutes, seconds: seconds})
 })
+
+setInterval(function() {
+	chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+		if (tabs.length === 0) {
+			console.log("all tabs are closed")
+		}
+		else {
+			console.log(tabs[0].url)
+		}
+	}) 
+}, 1000);
--- Source/javascripts/imgurTimer.js
+++ Source/javascripts/imgurTimer.js
@@ -1,34 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function () {
-// 	//only runs when window is open via DOMContentLoaded
-// 	// var counter = 0
-// 	// var addToCounter = function() {
-// 	// 	counter += 1
-// 	// 	document.getElementById('timeDisplay').innerText = counter
-// 	// }
-// 	// setInterval(function(){ addToCounter()}, 3000);
 
-// 	document.getElementById('timeDisplay').innerText = 'Hi'
-// })
-// setInterval(function() { 
-// 	var mytext = document.createTextNode('hi')
-// 	document.getElementById('timeDisplay').appendChild(mytext)
-// }, 1000)
-
-// chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-//   if (changeInfo.status == 'complete') {
-//   	var mytext = document.createTextNode('changed')
-// 	document.getElementById('timeDisplay').appendChild(mytext)
-//   }
-// })
-
-chrome.runtime.sendMessage({greeting: 'load'}, function(response) {
-	console.log(response.message)
-	return true
+chrome.runtime.sendMessage({greeting: 'give me the time'}, function(response) {
+	console.log(response)
 })
-
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//  	if (request.greeting === "heard you") {
-//    		var text = request.domText
-//    		document.getElementById('timeDisplay').appendChild(text)
-//   	}
-// });
